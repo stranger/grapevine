affd6f6edeadeb02f9f719361eb952a1ae4e4579
Stranger <magnum112@hotmail.se>
2018-01-10 04:50:05 +0100

    Spiced up the registration page

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '@app/shared/shared.module';
 
-import { MatInputModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatButtonModule, MatFormFieldModule } from '@angular/material';
 
 import { RegisterRoutingModule } from './register-routing.module';
+
 import { RegisterComponent } from './register.component';
 
 
@@ -13,9 +14,11 @@ import { RegisterComponent } from './register.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     SharedModule,
     MatInputModule,
     MatButtonModule,
+    MatFormFieldModule,
     RegisterRoutingModule
   ],
   declarations: [RegisterComponent],
